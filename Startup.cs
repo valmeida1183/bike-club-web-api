aef520e43c0330765d97c9385ebd063756d7869f
Vinicius Almeida <termobin@gmail.com>
2020-09-13 01:22:21 -0300

    Enable Exception Page Temporaly

--- Startup.cs
+++ Startup.cs
@@ -86,10 +86,11 @@ namespace BikeClub
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
+            app.UseDeveloperExceptionPage();
+            /* if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-            }
+            } */
 
             app.UseResponseCompression();
 
