900263a7601020421132567668730ec51250e37a
Vinicius Almeida <termobin@gmail.com>
2021-12-28 19:13:04 -0300

    Upgraded .Net version from 3.1 to 5

--- Startup.cs
+++ Startup.cs
@@ -1,22 +1,16 @@
-using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using BikeClub.Data;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.ResponseCompression;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 
@@ -43,28 +37,28 @@ namespace BikeClub
                 opt.EnableForHttps = true;
                 opt.Providers.Add<BrotliCompressionProvider>();
                 opt.MimeTypes = ResponseCompressionDefaults.MimeTypes
-                    .Concat(new[] { "application/json; charset=utf-8"});
-            });            
+                    .Concat(new[] { "application/json; charset=utf-8" });
+            });
 
             services.AddControllers();
 
             // Configs de Autenticação e Jwt
             var key = Encoding.ASCII.GetBytes(Settings.Secret);
-            services.AddAuthentication(a => 
+            services.AddAuthentication(a =>
             {
                 a.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
                 a.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-            }).AddJwtBearer(j => 
+            }).AddJwtBearer(j =>
             {
                 j.RequireHttpsMetadata = false;
                 j.SaveToken = true;
-                j.TokenValidationParameters = new TokenValidationParameters 
+                j.TokenValidationParameters = new TokenValidationParameters
                 {
                     ValidateIssuerSigningKey = true,
                     IssuerSigningKey = new SymmetricSecurityKey(key),
                     ValidateIssuer = false,
                     ValidateAudience = false
-                };        
+                };
             });
 
             // Config do EF com database em memória
@@ -73,9 +67,9 @@ namespace BikeClub
             //Config do EF com databse real
             services.AddDbContext<DataContext>(opt => opt.UseSqlServer(Configuration.GetConnectionString("connectionString")));
 
-            services.AddSwaggerGen(c => 
+            services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo 
+                c.SwaggerDoc("v1", new OpenApiInfo
                 {
                     Title = "Bike Club Api",
                     Version = "v1"
@@ -104,7 +98,7 @@ namespace BikeClub
             app.UseHttpsRedirection();
 
             app.UseSwagger();
-            app.UseSwaggerUI(c => 
+            app.UseSwaggerUI(c =>
             {
                 c.SwaggerEndpoint("/swagger/v1/swagger.json", "Bike Club Api Version: 1");
             });
