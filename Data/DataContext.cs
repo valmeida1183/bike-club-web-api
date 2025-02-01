92227e63841c3b958b2462b24928bc9e6ac792f9
Vinicius Lopes de Almeida <termobin@gmail.com>
2025-01-31 23:50:31 -0300

    Updated to .Net 9

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -6,11 +6,9 @@ namespace BikeClub.Data
 {
     public class DataContext : DbContext
     {
-#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
-        public DataContext(DbContextOptions<DataContext> options) : base(options)
-#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
-        {
 
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
